22f855fe533c332b5f9b74f4903fe40e601d01dd
Alex Fix <dasdias@yandex.ru>
2023-01-31 10:41:38 +0300

    lesson01

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 let colors = require('colors');
-console.log(colors);
+
+const initValue = process.argv.slice(2);
+const itnValue = initValue.map((elem) => {
+	return parseInt(elem);
+})
+const [num1, num2] = itnValue;
+
+if (isNaN(num1) || isNaN(num2)) {
+	console.log(colors.red("Arguments not a number"));
+	process.exit(0);
+}
+
+function isPrime(num) {
+	if (num < 2) return false;
+	for (var i = 2; i < num; i++) {
+		if (num % i == 0)
+			return false;
+	}
+	return true;
+}
+for (var i = num1; i < num2; i++) {
+	if (isPrime(i)) console.log(i);
+}
+// console.log(num1 % num2);
