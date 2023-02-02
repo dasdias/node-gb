87a1942c3e77afb78021ec72c0ebf179c88c4c14
Alex Fix <dasdias@yandex.ru>
2023-02-02 08:59:19 +0300

    lesson01

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let colors = require('colors');
-
+let countColor = 1;
+let flag = false;
 const initValue = process.argv.slice(2);
 const itnValue = initValue.map((elem) => {
 	return parseInt(elem);
@@ -10,6 +11,20 @@ if (isNaN(num1) || isNaN(num2)) {
 	console.log(colors.red("Arguments not a number"));
 	process.exit(0);
 }
+function colorNum(num) {
+	if (countColor === 1) {
+		countColor++;
+		return colors.green(num);
+	}
+	if (countColor === 2) {
+		countColor++
+		return colors.yellow(num);
+	}
+	if (countColor === 3) {
+		countColor = 1
+		return colors.red(num);
+	}
+}
 
 function isPrime(num) {
 	if (num < 2) return false;
@@ -19,7 +34,15 @@ function isPrime(num) {
 	}
 	return true;
 }
+
+
 for (var i = num1; i < num2; i++) {
-	if (isPrime(i)) console.log(i);
+	if (isPrime(i)) {
+		console.log(colorNum(i));
+		flag = true;
+	}
 }
-// console.log(num1 % num2);
+
+if (!flag) {
+	console.log(colors.red('There are no prime numbers'));
+}
