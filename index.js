5051672f18f4897b2113a14519193a04f13ed686
Alex Fix <dasdias@yandex.ru>
2023-02-08 10:24:12 +0300

    lesson03

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
 const fs = require('fs');
-const url = 'https://docs.google.com/document/d/1uuKKxercOrN4uXx6chCypM7XSXTSRY27Y19KCebfL-s/edit';
-const myURL = new URL(url);
-console.log(myURL)
-// let readFile;
+const steam = require('node:stream');
 
+const filePath = './access_tmp.log';
 
-// fs.readFile('access.log', 'utf-8', (err, data) => {
-// 	console.log(err);
-// 	console.log(data);
-// 	readFile = data;
-// 	console.log('readFile ', readFile)
-// 	fs.writeFile('write-file.log', '\n', { flag: 'a', encoding: 'utf-8' }, (err, data) => console.log(err));
-// 	fs.writeFile('write-file.log', readFile, { flag: 'a', encoding: 'utf-8' }, (err, data) => console.log('writeFile err ', err));
-// })
-// async function getData() {
+const readStream = fs.createReadStream(filePath, 'utf8');
 
-// 	await fetch(url).then((res) => {
-// 		console.log(res);
-// 		return res
-// 	})
-// 		.then((data) => {
-// 			console.log(data)
-// 		})
-// }
+const transformStreamFirst = new steam.Transform({
+	transform(chunk, encoding, callback) {
+		const firstIpChunk = chunk.toString().trim().split('\n').filter(item => item.includes('89.123.1.41')).join('').toString();
+		this.push(firstIpChunk)
 
-// getData();
+		callback()
+	}
+})
+const transformStreamSecond = new steam.Transform({
+	transform(chunk, encoding, callback) {
+		const SecondIpChunk = chunk.toString().trim().split('\n').filter(item => item.includes('34.48.240.111')).join('').toString();
+		this.push(SecondIpChunk)
 
-const readStream = fs.createReadStream(myURL.href, 'utf8');
-
-readStream.on('data', (chunk) => {
-	console.log('data', chunk);
-	console.log(chunk);
+		callback()
+	}
 })
 
-// readStream.on('end', () => console.log('File reading finished'));
-// readStream.on('error', () => console.log(err));
+const writeStreamFirstIp = fs.createWriteStream('./89.123.1.41_requests.log')
+const writeStreamSecondIp = fs.createWriteStream('./34.48.240.111_requests.log')
 
+readStream
+	.pipe(transformStreamFirst)
+	.pipe(writeStreamFirstIp)
 
-// const log1 = '127.0.0.1 - - [30/Jan/2021:11:11:20 -0300] "POST /foo HTTP/1.1" 200 0 "-" "curl/7.47.0"';
-// const writeStream = fs.createWriteStream('./write-file.log', {
-// 	flags: 'a',
-// 	encoding: 'utf8'
-// });
-// writeStream.write('\n');
-// writeStream.write(log1);
-// writeStream.end(() => console.log('File writing finished'));
+readStream
+	.pipe(transformStreamSecond)
+	.pipe(writeStreamSecondIp)
